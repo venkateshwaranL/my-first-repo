bf82fbc23898407d64c25c36c02a653f1f84c910
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-09 22:08:48 +0530

    Venkatshwaran-even-odd

---
+++ exercise/EvenOdd.java
@@ -0,0 +1,17 @@
+package exercise;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        int num = 5;
+       
+        if(num % 2 == 0)
+       
+        System.out.println(num + ” is even.”);
+       
+        else
+       
+        System.out.println(num + ” is odd.”);
+       
+        }
+}
