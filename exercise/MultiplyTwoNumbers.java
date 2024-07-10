a9cd1789c8c016810d8efeff3c4eee3a8a26d678
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-10 22:28:44 +0530

    Venkateshwaran-mul-number

---
+++ exercise/MultiplyTwoNumbers.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class MultiplyTwoNumbers {
+    public class MultiplyTwoNumbers {
+
+        public static void main(String[] args) {
+       
+        double first = 2.5, second = 4.5;
+       
+        double product = first * second;
+       
+        System.out.println(“The product is: ” + product);
+       
+        }
+}
