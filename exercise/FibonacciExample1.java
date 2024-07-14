9f274a1ca449e883a2f7a4aa0703fd5b51a9728f
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-14 23:23:21 +0530

    Venkateshwaran-modified-file

--- exercise/FibonacciExample1.java
+++ exercise/FibonacciExample1.java
@@ -3,15 +3,15 @@ package exercise;
 public class FibonacciExample1 {
     public static void main(String args[])  
      {    
-      int num1=0,num2=1,num3,i,count=10;    
-      System.out.print(num1+" "+num2);//printing 0 and 1    
+      int number1=0,number2=1,number3,i,count=10;    
+      System.out.print(number1+" "+number2);//printing 0 and 1    
          
       for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
       {    
-       num3=num1+num2;    
-       System.out.print(" "+num3);    
-       num1=num2;    
-       num2=num3;    
+       number3=number1+number2;    
+       System.out.print(" "+number3);    
+       number1=number2;    
+       number2=number3;    
       }    
        
      }
