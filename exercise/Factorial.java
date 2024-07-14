5c94ed68ec7060b339c16e860d9fcb6e2606ce06
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-14 23:21:26 +0530

    Venkateshwaran-modified-file

--- exercise/Factorial.java
+++ exercise/Factorial.java
@@ -3,15 +3,15 @@ package exercise;
 public class Factorial {
     public static void main(String[] args) {
 
-        int num = 5, factorial = 1;
+        int number = 5, factorial = 1;
        
-        for(int i = 1; i <= num; ++i) {
+        for(int i = 1; i <= number; ++i) {
        
         factorial *= i;
        
         }
        
-        System.out.println(“Factorial of ” + num + ” is: ” + factorial);
+        System.out.println(“Factorial of ” + number + ” is: ” + factorial);
        
         }
 }
