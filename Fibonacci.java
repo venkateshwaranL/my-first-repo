a5e3552dd988e9ab70fd19ad30c67ea080aed280
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-15 22:23:56 +0530

    Venkateshwaran-modified-file

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,15 +1,15 @@
 class Fibonacci{  
 public static void main(String args[])  
 {    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
+ int num1=0,num2=1,num3,i,count=10;    
+ System.out.print(num1+" "+num2);//printing 0 and 1    
     
  for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
  {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
+  num3=num1+num2;    
+  System.out.print(" "+num3);    
+  num1=num2;    
+  num2=num3;    
  }    
   
 }}  
