e0bf28cead5ea9c152450efb4c7b7c8257380ae6
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-18 17:18:46 +0530

    Venkateshwaran-modified-file

--- PrimeExample.java
+++ PrimeExample.java
@@ -1,12 +1,12 @@
 public class PrimeExample{    
  public static void main(String args[]){    
-  int i,num=0,flag=0;      
+  int i,number=0,flag=0;      
   int n=3;//it is the number to be checked    
-  num=n/2;      
+  number=n/2;      
   if(n==0||n==1){  
    System.out.println(n+" is not prime number");      
   }else{  
-   for(i=2;i<=num;i++){      
+   for(i=2;i<=number;i++){      
     if(n%i==0){      
      System.out.println(n+" is not prime number");      
      flag=1;      
