a958e4166ce1ab68c5464651eb17b5b29b4903f7
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-18 17:17:10 +0530

    Venkateshwaran-modified-file

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -2,12 +2,12 @@ public class ReverseNumber
 {  
 public static void main(String[] args)   
 {  
-int number = 987654, reverse = 0;  
+int num = 987654, reverse = 0;  
 while(number != 0)   
 {  
-int remainder = number % 10;  
+int remainder = num % 10;  
 reverse = reverse * 10 + remainder;  
-number = number/10;  
+num = num/10;  
 }  
 System.out.println("The reverse number is: " + reverse);  
 }  
