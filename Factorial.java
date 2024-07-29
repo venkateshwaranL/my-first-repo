476a876c984310b3f6333fc40acf842b72b818b5
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-07-29 23:19:04 +0530

    VEnkateshwaran-modified-var-name

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,10 @@
 class FactorialExample{  
  public static void main(String args[]){  
-  int i,fact=1;  
+  int i,f=1;  
   int num=5;//It is the number to calculate factorial    
   for(i=1;i<=num;i++){    
-      fact=fact*i;    
+      f=f*i;    
   }    
-  System.out.println("Factorial of "+num+" is: "+fact);    
+  System.out.println("Factorial of "+num+" is: "+f);    
  }  
 }
