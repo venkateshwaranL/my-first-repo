5ad120b856730c966290a26d65d5f4129053fff7
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-08-01 23:28:51 +0530

    Venkateshwaran-modified-value

--- ReverseNumberExample1.java
+++ ReverseNumberExample1.java
@@ -2,7 +2,7 @@ public class ReverseNumberExample1
 {  
 public static void main(String[] args)   
 {  
-int number = 987654, reverse = 0;  
+int number = 9876543, reverse = 0;  
 while(number != 0)   
 {  
 int remainder = number % 10;  
