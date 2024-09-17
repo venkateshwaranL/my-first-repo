911dee38c2ed03ae30fd2abff63b9650f09347e2
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-09-17 21:40:51 +0530

    Venkateshwaran-modified-file

--- exercise/Factorial.java
+++ exercise/Factorial.java
@@ -3,7 +3,7 @@ package exercise;
 public class Factorial {
     public static void main(String[] args) {
 
-        int number = 5, factorial = 1;
+        int number = 6, factorial = 1;
        
         for(int i = 1; i <= number; ++i) {
         factorial *= i;
