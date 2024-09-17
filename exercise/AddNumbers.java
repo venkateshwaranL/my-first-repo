9488ba3b4f45cba9294edb0ef71de95a46f9f96a
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-09-17 21:40:04 +0530

    Venkateshwaran-mdified-file

--- exercise/AddNumbers.java
+++ exercise/AddNumbers.java
@@ -2,13 +2,9 @@ package exercise;
 
 public class AddNumbers {
     public static void main(String[] args) {
-
         int num1 = 5, num2 = 10, sum;
-       
         Totalsum = num1 + num2;
-       
         System.out.println(“Sum of ” + num1 + ” and ” + num2 + ” is: ” + Totalsum);
-       
         }
        
 }
