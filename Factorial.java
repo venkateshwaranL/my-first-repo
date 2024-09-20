250b44e1f78edb5e5aba9fb8949444bbf4a1c75d
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-09-20 16:41:01 +0530

    Venkateshwaran-modified-file

--- Factorial.java
+++ Factorial.java
@@ -1,8 +1,9 @@
-class FactorialExample{  
- public static void main(String args[]){  
+class FactorialExample {  
+ public static void main(String args[]) {  
   int i,f=1;  
   int num=5;//It is the number to calculate factorial    
-  for(i=1;i<=num;i++){    
+  for(i=1;i<=num;i++)
+  {    
       f=f*i;    
   }    
   System.out.println("Factorial of "+num+" is: "+f);    
