71f5b62038846324baae9da55a9a40a3e5fb2b5c
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-09-20 16:40:26 +0530

    Venkateshwaran-modified-file

--- CountCharacter.java
+++ CountCharacter.java
@@ -1,14 +1,12 @@
 public class CountCharacter {
     public static void main(String[] args) {    
         String statement = "The best of both worlds";    
-        int count = 0;    
-            
+        int count = 0;     
         //Counts each character except space    
         for(int i = 0; i < statement.length(); i++) {    
             if(statement.charAt(i) != ' ')    
                 count++;    
-        }    
-            
+        }     
         //Displays the total number of characters present in the given string    
         System.out.println("Total number of characters in a string is: " + count);    
     }    
