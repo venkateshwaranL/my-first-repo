5db726db3d63dd8e4f859d022f28c04df8a7c61c
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2024-09-23 23:16:42 +0530

    Venkateshwaran-modified-file

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -6,7 +6,8 @@ public static void main(String[] args) {
     int remainder = num % 10;  
     reverse = reverse * 10 + remainder;  
     num = num/10;  
-  }  
+  } 
+   
    System.out.println("The reverse number is: " + reverse);  
  }  
 } 
