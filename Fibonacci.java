93d46f5f416bb067489e5f880e2fdefb83797f6e
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-04-13 18:24:32 +0530

    Vekatesh-update-file

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,8 @@
 class Fibonacci {  
 public static void main(String args[]) {    
  int num1=0,num2=1,num3,i,count=10;    
- System.out.print(num1+" "+num2);//printing 0 and 1    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ System.out.print(num1+" "+num2);
+ for(i=2;i<count;++i)   
  {    
   num3=num1+num2;    
   System.out.print(" "+num3);    
