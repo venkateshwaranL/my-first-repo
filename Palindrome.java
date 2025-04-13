cd4b5e68d8b2dc1d091d6e3e21f360e8eaa04aed
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-04-13 18:24:46 +0530

    Vekatesh-update-file

--- Palindrome.java
+++ Palindrome.java
@@ -2,7 +2,6 @@ class PalindromeExample{
  public static void main(String args[]){  
   int r,sum=0,temp;    
   int n=454;//It is the number variable to be checked for palindrome  
-  
   temp=n;    
   while(n>0){    
    r=n%10;  //getting remainder  
@@ -14,5 +13,4 @@ class PalindromeExample{
   else    
    System.out.println("not palindrome");    
 }  
-}  
-
+} 
