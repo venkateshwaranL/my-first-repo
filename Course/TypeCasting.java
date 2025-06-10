e21a327c7f919fa544bb4e6e541bb5becc6c7b20
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-10 23:34:16 +0530

    Venkatesh-add-typecasting-file

---
+++ Course/TypeCasting.java
@@ -0,0 +1,16 @@
+class TypeCasting {
+    public static void main(String args[])
+    {
+        //byte b = 127;
+        int a = 257;
+        byte k = (byte) a;
+
+        float f = 5.6f;
+        int t = (int) f ;
+
+        byte v = 10;
+        byte b = 30;
+        int result = v*b; 
+        System.out.println(result);
+    }
+}
