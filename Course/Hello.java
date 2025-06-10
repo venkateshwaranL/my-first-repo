98f96052f65dbebe5a9817ffad6c96790788b258
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-10 23:56:23 +0530

    Venkatesh-add-first-file

--- Course/Hello.java
+++ Course/Hello.java
@@ -2,7 +2,7 @@ class Hello {
     public static void main(String args[]) {
         int a =5;
         float b=5.6f;
-        double c =8.55;
+        double c =8.88;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
