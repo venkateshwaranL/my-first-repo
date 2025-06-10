0b8d3f9232e099ecccaa894602d9b181391c06d5
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-10 17:14:14 +0530

    Venkatesh-add-sample-file

--- Course/Hello.java
+++ Course/Hello.java
@@ -1,5 +1,11 @@
 class Hello {
     public static void main(String args[]) {
+        int a =5;
+        float b=5.6f;
+        double c =8.55;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         System.out.println("Hello World");
     }
 }
