0b8de77e1a8161f0c3f50bcd888537e3c7501a0b
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-10 19:34:34 +0530

    Venkatesh-add-datatype-file

---
+++ Course/DataType.java
@@ -0,0 +1,17 @@
+class DataType {
+    public static void main(String args[])
+    {
+        int num = 57;
+        byte by = -127;
+        short sh = 754;
+        long lo = 87975l;
+
+        float fl = 85.4f;
+        double d = 8547.555;
+
+        char c = 's';
+        String str = "Venkateshwaran";
+        boolean blt = true ;
+
+    }
+}
