676e856a19a3505eb605db6643a7418359992b1a
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-11 09:08:57 +0530

    Venkatesh-add-operators-file

---
+++ Course/Operators.java
@@ -0,0 +1,12 @@
+class Operators {
+    public static void main(String args[]){
+        short a = 50 ;
+        short b = 40 ;
+        short x = 100 ;
+        short y = 120 ;
+        boolean result = a<b || x<y ;
+        boolean not = !result;
+        System.out.println(result);
+        System.out.println(not);
+    }
+}
