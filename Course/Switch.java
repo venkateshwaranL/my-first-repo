8440b18e68599aafa69496f0fb303b5914ed2121
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-11 19:43:05 +0530

    Venkatesh-add-switch-file

---
+++ Course/Switch.java
@@ -0,0 +1,31 @@
+class Switch{
+    public static void main(String[] v){
+        int n = 3;
+        switch(n){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Firday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter the vaild number");
+                
+        }
+    }
+}
