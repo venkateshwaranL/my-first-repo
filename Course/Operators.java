4ea330f1a8ef19edaba50af1df70d913e8eeea36
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-11 09:45:17 +0530

    Venkatesh-modified-operatord-file

--- Course/Operators.java
+++ Course/Operators.java
@@ -2,6 +2,7 @@ class Operators {
     public static void main(String args[]){
         short a = 50 ;
         short b = 40 ;
+        short c = 90 ;
         short x = 100 ;
         short y = 120 ;
         boolean result = a<b || x<y ;
@@ -11,10 +12,12 @@ class Operators {
         }else{
         System.out.println("Bye");
         }
-        if(a>b){
-            System.out.println("A is Greather than B");
+        if(a>b && a>c){
+            System.out.println("A is Greather than B & C");
+        }else if(b>a && b>c){
+            System.out.println("B is Greather than A & c");
         }else{
-            System.out.println("B is Greather than A");
+            System.out.println("C is Greater than A & B");
         }
         System.out.println(result);
         System.out.println(not);
