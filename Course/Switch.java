b3a055d3cea83e5de96c2058270d2ebb4af41d48
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-16 23:52:56 +0530

    Venkatesh-modified-file

--- Course/Switch.java
+++ Course/Switch.java
@@ -1,6 +1,6 @@
 class Switch{
     public static void main(String[] v){
-        int n = 3;
+        int n = 2;
         switch(n){
             case 1:
                 System.out.println("Monday");
