14f5ac890337e8e28ef35814105343e4ca00c86d
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-06-16 23:53:32 +0530

    Venkatesh-modified-file

--- Course/Ternary.java
+++ Course/Ternary.java
@@ -2,7 +2,7 @@ class Ternary {
     public static void main(String v[]){
         int n = 6,result ;
         //int result;
-        result = (n%2==0)?10:20;
+        result = (n%2==0)?20:10;
         System.out.println(result);
     }
 }
