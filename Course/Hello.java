a2714ed71a1200433442eb914c281f0c0ee243e6
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-07-13 23:14:11 +0530

    Venkatesh-mod-file

--- Course/Hello.java
+++ Course/Hello.java
@@ -2,7 +2,7 @@ class Hello {
     public static void main(String args[]) {
         int a =55;
         float b=5.6f;
-        double c =8.88;
+        double c =8.888;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
