b591c649d5e174aabbc10e80551b726cb7c7a2f6
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-07-24 13:00:05 +0530

    Venkatesh-add-file

---
+++ Course/Max2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Max2 {
+
+public static void main(String[] args){
+
+int[] arr = {5, 8, 1, 4, 10, 7};
+int firstMax = Integer.MIN_VALUE;
+int secondMax = Integer.MIN_VALUE;
+
+for (int num : arr) {
+    if (num > firstMax) {
+        secondMax = firstMax;
+        firstMax = num;
+    } else if (num > secondMax && num != firstMax) {
+        secondMax = num;
+    }
+}
+System.out.println("Second Maximum: " + secondMax);
+}
+}
