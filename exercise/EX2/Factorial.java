f6cba754d5ab834af221e66cd2fac9a6e0e8cdc2
Venkateshwaran lakshmi Narayanan <lvenkateshwaran9@gmail.com>
2025-08-05 23:23:40 +0530

    Venkatesh-modfied-file

--- exercise/EX2/Factorial.java
+++ exercise/EX2/Factorial.java
@@ -1,7 +1,7 @@
 class FactorialExample {  
  public static void main(String args[]) {  
   int i,f=1;  
-  int num=5;//It is the number to calculate factorial    
+  int num=5;    
   for(i=1;i<=num;i++)
   {    
       f=f*i;    
